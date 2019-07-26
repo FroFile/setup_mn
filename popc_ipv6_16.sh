8c98d42dbe6f86d6ace567880a923c9ef2b56732
FroFile <39367696+FroFile@users.noreply.github.com>
2019-07-26 11:37:35 +0900

    Update and rename popcsetupip6 to popc_ipv6_16.sh

---
+++ popc_ipv6_16.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+BLUE="\033[0;34m"
+YELLOW="\033[0;33m"
+CYAN="\033[0;36m" 
+PURPLE="\033[0;35m"
+RED='\033[0;31m'
+GREEN="\033[0;32m"
+NC='\033[0m'
+MAG='\e[1;35m'
+
+echo -e "${GREEN}Searching and removing old $COIN_NAME files and configurations${NC}"
