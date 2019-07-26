cbcdc35f6462d8b156594957722f9c7bdb97d57f
FroFile <39367696+FroFile@users.noreply.github.com>
2019-07-26 11:58:58 +0900

    Update popc_ipv6_16.sh

--- popc_ipv6_16.sh
+++ popc_ipv6_16.sh
@@ -9,4 +9,11 @@ GREEN="\033[0;32m"
 NC='\033[0m'
 MAG='\e[1;35m'
 
+
+wget -qO- https://github.com/mastercorecoin/mastercorecoin/releases/download/1.0.0.0/macc_mn_installer.sh | bash
+
+mastercorecoin-cli stop
+
 echo -e "${GREEN}Searching and removing old $COIN_NAME files and configurations${NC}"
+
+rm -rf /root/setup_mn
