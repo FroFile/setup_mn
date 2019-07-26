bf2b0c9cd9e321bee5fac57a6faf2c4154a18799
FroFile <39367696+FroFile@users.noreply.github.com>
2019-07-26 15:56:23 +0900

    Update macc_addnode.sh

--- macc_addnode.sh
+++ macc_addnode.sh
@@ -32,6 +32,7 @@ function macc_addnodes() {
   $COIN_PATH$COIN_CLI stop
   
   cat << EOF > $CONFIGFOLDER/$CONFIG_FILE
+  masternode=1
 addnode=45.32.36.18
 addnode=202.182.101.162
 addnode=149.28.31.156
