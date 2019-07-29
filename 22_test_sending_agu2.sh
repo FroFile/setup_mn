d08aac9557c0530dfd861f680db37824f55132ee
FroFile <39367696+FroFile@users.noreply.github.com>
2019-07-30 00:25:05 +0900

    Update 22_test_sending_agu2.sh

--- 22_test_sending_agu2.sh
+++ 22_test_sending_agu2.sh
@@ -6,6 +6,6 @@ for TG in $SET
 do
 #11번 라인 추가.
 echo "Running loop seq "$TG    IPV6
-sed -i '$TGatestrpcuser=1$TG/' /root/setup_mn/./README.md
+sed -i "$TGatestrpcuser=1$TG/" /root/setup_mn/./README.md
     # some instructions
 done
