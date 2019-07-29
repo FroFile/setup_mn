998d4388eed3f43e375548ea38a114e91434f738
FroFile <39367696+FroFile@users.noreply.github.com>
2019-07-29 20:12:48 +0900

    Update 7_test_add_ipv6_macc.sh

--- 7_test_add_ipv6_macc.sh
+++ 7_test_add_ipv6_macc.sh
@@ -12,6 +12,7 @@ COIN_NAME='mastercorecoin'
 #COIN_EXPLORER='http://chain.cdmcoin.org'
 COIN_PORT=29871
 RPC_PORT=29872
+SET_NUM=15
 
 BLUE="\033[0;34m"
 YELLOW="\033[0;33m"
@@ -26,7 +27,7 @@ MAG='\e[1;35m'
 IPV4=$1
 IPV6=$2
 
-function check_system() {
+function check_system_ipv4() {
     echo -e "* Checking system for compatibilities"
 if [[ $EUID -ne 0 ]]; then
    echo -e "${RED}$0 must be run as root.${NC}"
@@ -49,15 +50,54 @@ NODEIP=$(curl -s4 icanhazip.com)
 #IPv6는 read로 전달하도록 해야할듯.
 
 if [[ $NODEIP != $IPV4 ]]; then
-   echo -e "${RED} IPv4 and IPv6 must match.${NC}"
+   echo -e "${RED} IPv4 and IPv6 must match. You check IPc4.${NC}"
    exit 1
 fi
 
-echo -e "* Checking system *** OK"
+echo -e "${RED}*** Checking system and IPv4 *** "
 }
 
-echo -e "${RED}================================================================================================================================${NC}"
-echo -e "${RED}================================================================================================================================${NC}"
+echo -e "${RED}==========================================================================${NC}"
+echo -e "${RED}==========================================================================${NC}"
+
+
+SET=$(seq 0 $SET_NUM)
+for i in $SET
+do
+echo "Running loop seq "$i
+    # some instructions
+done
+
+
+function add_ipv6() {
+
+#ipv6가 추가되어있냐?
+if [[ $NODEIP != $IPV4 ]]; then
+ echo -e "${RED} IPv4 and IPv6 must match. You check IPc4.${NC}"
+ exit 1
+fi
+
+iface ens3 inet6 static
+13:address 2001:19f0:7002:137::1
+14:netmask 64
+15:up /sbin/ip -6 addr add dev ens3 2001:19f0:7002:137::2
+16:up /sbin/ip -6 addr add dev ens3 2001:19f0:7002:137::3
+17:up /sbin/ip -6 addr add dev ens3 2001:19f0:7002:137::4
+18:up /sbin/ip -6 addr add dev ens3 2001:19f0:7002:137::5
+
+
+#11번 라인 삭제
+  sed -i '11d' /etc/network/interfaces
+  
+for ((AN1=0; AN1<=; n2++))
+#11번 라인 추가.
+  sed -i '11aiface ens3 inet6 static'
+#12번 라인 추가
+  
+}
+
+echo -e "${RED}==========================================================================${NC}"
+echo -e "${RED}==========================================================================${NC}"
 
 #read -p "IPv6?" NODEIPv6
 #echo -e "${NODEIPv6}1"  
@@ -90,10 +130,10 @@ EOF
   sleep 2
   $COIN_PATH$COIN_DAEMON -daemon
   sleep 2
- echo -e "${RED}addnode work is done${NC}"
+  echo -e "${RED}addnode work is done${NC}"
 
   $COIN_PATH$COIN_CLI getinfo
 }
 
-check_system
+check_system_ipv4
 #edit_macc_add_IPv6
