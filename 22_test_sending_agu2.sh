52570a49f4d382fba21e0d3f5f1728d5065475e2
FroFile <39367696+FroFile@users.noreply.github.com>
2019-07-30 00:23:24 +0900

    Update 22_test_sending_agu2.sh

--- 22_test_sending_agu2.sh
+++ 22_test_sending_agu2.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 SET=$(seq 0 13)
-for i in $SET
+for TG in $SET
 #반복문
 do
 #11번 라인 추가.
-echo "Running loop seq "$i    IPV6
-sed -i '$iatestrpcuser=1$i/' /root/setup_mn/./README.md
+echo "Running loop seq "$TG    IPV6
+sed -i '$TGatestrpcuser=1$TG/' /root/setup_mn/./README.md
     # some instructions
 done
