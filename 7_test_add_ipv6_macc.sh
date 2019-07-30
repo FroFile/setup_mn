c0c1967157390a20bb700feb808b6092e823ab77
FroFile <39367696+FroFile@users.noreply.github.com>
2019-07-30 09:26:15 +0900

    Update 7_test_add_ipv6_macc.sh

--- 7_test_add_ipv6_macc.sh
+++ 7_test_add_ipv6_macc.sh
@@ -61,7 +61,7 @@ echo -e "${RED}*** Checking system and IPv4 OK *** "
 echo -e "${RED}==========================================================================${NC}"
 echo -e "${RED}==========================================================================${NC}"
 
-function add_ipv6() {
+function add_reboot_ipv6() {
 #ipv6가 추가되어 있는지를 어떻게 알 수 있을까... 어떻게 추가할까...
 #if [[ ~~~ != ~~~~ ]]; 
 
@@ -69,7 +69,7 @@ function add_ipv6() {
   sed -i '11d' /etc/network/interfaces
   
 #IPv6 추가하기. 맨끝에 변하는 자리 하나는 빼놓고 넣어주기.
-  cat << EOF >> /root/setup_mn/./README.md
+  cat << EOF >> $CONFIGFOLDER/$CONFIG_FILE
 iface ens3 inet6 static
 address ${IPV6V}1
 netmask 64
@@ -98,11 +98,79 @@ sleep 3
 #추가했던 네트워크들이 확인되는지 체크하기
 ip addr show ens3
 sleep 3
+} 
+
+echo -e "${RED}==========================================================================${NC}"
+echo -e "${RED}==========================================================================${NC}"
+
+function edit_macc_addnode() {
+#복사전... 이 부분 POPC는 달라져야 함...################
+sed -i '3d' $CONFIGFOLDER/$CONFIG_FILE
+
+##고정값 추가 및 addnode 
+##아직 정지안했음.
+  cat << EOF >> $CONFIGFOLDER/$CONFIG_FILE
+ maxconnections=256
+ logtimestamps=1
+ port=29871
+ 
+addnode=45.32.36.18
+addnode=202.182.101.162
+addnode=149.28.31.156
+addnode=45.63.121.56
+addnode=149.28.19.210
+addnode=107.191.53.220
+addnode=45.77.183.241
+addnode=45.76.52.233
+addnode=198.13.37.49
+addnode=45.77.29.239
+addnode=128.199.171.192
+addnode=45.77.21.70
+addnode=198.13.38.119
+addnode=45.32.39.247
+addnode=95.179.154.9
+addnode=157.230.123.11
+addnode=104.248.141.211
+addnode=178.128.54.41
+addnode=204.48.26.40
+addnode=207.154.201.240
+addnode=159.89.151.147
+addnode=167.99.206.80
+addnode=138.68.103.119
+addnode=134.209.225.16
+addnode=159.65.139.78
+addnode=68.183.64.70
+addnode=104.248.157.119:34652
+addnode=104.248.39.113:29871
+addnode=112.162.233.135:50514
+addnode=113.10.36.11:60418
+addnode=128.199.155.59:55256
+addnode=128.199.167.13:29871
+addnode=128.199.171.192:37988
+addnode=128.199.175.9:42000
+addnode=128.199.251.99:47266
+addnode=134.209.108.46:60394
+addnode=134.209.108.51:49510
+addnode=134.209.225.16:58492
+addnode=134.209.233.131:52286
+addnode=134.209.240.245:51216
+addnode=134.209.246.108:42116
+EOF
+
+  sleep 2
+  echo -e "${RED}addnode work is done${NC}"
+
+  sleep 2
+  $COIN_PATH$COIN_CLI getinfo
 }
 
 echo -e "${RED}==========================================================================${NC}"
 echo -e "${RED}==========================================================================${NC}"
 
+function copy_dir_add_conf() {
+
+
+}
 #read -p "IPv6?" NODEIPv6
 #echo -e "${NODEIPv6}1"  
 #sed -i '3atestrpcuser=1/' /root/setup_mn/./README.md
@@ -123,22 +191,11 @@ echo -e "223456789"
 echo -e "${GENKEY}"
 
 
-function edit_macc_add_IPv6() {
-  $COIN_PATH$COIN_CLI stop
-  sleep 2
-  
-  cat << EOF >> $CONFIGFOLDER/$CONFIG_FILE
- 
-EOF
 
-  sleep 2
-  $COIN_PATH$COIN_DAEMON -daemon
-  sleep 2
-  echo -e "${RED}addnode work is done${NC}"
 
-  $COIN_PATH$COIN_CLI getinfo
-}
 
 check_system_ipv4
-add_ipv6
-#edit_macc_add_IPv6
+add_reboot_ipv6
+edit_macc_addnode
+copy_dir_add_conf
+
